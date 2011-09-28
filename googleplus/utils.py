9ef0f210288df5adc9a592e1e96d3ff7f10dc493
utek <mail@utek.pl>
2011-09-28 09:44:45 +0200

    cleaning

--- googleplus/utils.py
+++ googleplus/utils.py
@@ -13,27 +13,27 @@ def api(path, params, method="GET"):
     # Construct the API url.
     base_url = 'https://www.googleapis.com/plus/v1/'
     url = urlparse.urljoin(base_url, path)
-    
-    # Invoke the Google+ API with the specified method. 
+
+    # Invoke the Google+ API with the specified method.
     if method.upper() == "GET":
         req = urllib.urlopen(url + '?' + urllib.urlencode(params))
     elif method.upper() == "POST":
         req = urllib.urlopen(url, urllib.urlencode(params))
-    
+
     response = req.read()
     response_dict = json.loads(response)
-    
+
     return response_dict
 
 def get_registration_backend(backend_str=None):
-    """ 
+    """
     Get's the registration backend instance, the default backend if no backend is defined
     by settings.GOOGLEPLUS_REGISTRATION_BACKEND.
     If backend_str takes the overhand of settings.GOOGLEPLUS_REGISTRATION_BACKEND if defined.
     """
 
     backend = None
-    # Used when the default backend needs to be loaded, overwritten elsewhise 
+    # Used when the default backend needs to be loaded, overwritten elsewhise
     module, cls = "googleplus.backends.default", "DefaultBackend"
     import pdb
     pdb.set_trace()
@@ -61,14 +61,6 @@ def get_registration_backend(backend_str=None):
         raise ImproperlyConfigured("Error loading registration backend %s. GOOGLE_REGISTRATION_BACKEND is \
                                     improperly configured." % backend_str)
     return backend
-        
 
-    #t#r#y#:
-        #f#r#o#m #r#e#g#i#s#t#r#a#t#i#o#n#.#b#a#c#k#e#n#d#s #i#m#p#o#r#t #g#e#t#_#b#a#c#k#e#n#d
-        #t#r#y#:
-            #b#a#c#k#e#n#d #= #g#e#t#_#b#a#c#k#e#n#d#(#s#e#t#t#i#n#g#s#.#G#O#O#G#L#E#_#R#E#G#I#S#T#R#A#T#I#O#N#_#B#A#C#K#E#N#D#)
-        #e#x#c#e#p#t#:
-            #r#a#i#s#e #V#a#l#u#E#r#r#o#r#(#"#C#a#n#n#o#t #g#e#t #d#j#a#n#g#o#-#r#e#g#i#s#t#r#a#t#i#o#n #b#a#c#k#e#n#d #f#r#o#m #s#e#t#t#i#n#g#s#.#R#E#G#I#S#T#R#A#T#I#O#N#_#B#A#C#K#E#N#D#"#)
-    #e#x#c#e#p#t #I#m#p#o#r#t#E#r#r#o#r#, #e#:
-        #b#a#c#k#e#n#d #= #N#o#n#e
+
 
--- googleplus/views.py
+++ googleplus/views.py
@@ -18,7 +18,7 @@ from googleplus.models import GooglePlusUser
 from googleplus.utils import get_registration_backend
 
 GOOGLEPLUS_LOGIN_URL = '/googleplus/login/'
-REDIRECT_URI = urlparse.urljoin( 
+REDIRECT_URI = urlparse.urljoin(
         #     'http://' + Sit.objects.get_current().domain, GOOGLEPLUS_LOGIN_URL
         'http://localhost:8000', GOOGLEPLUS_LOGIN_URL
 )
@@ -30,9 +30,9 @@ def login_handler(request, backend=None):
 
     if request.user.is_authenticated():
         return HttpResponseRedirect(settings.LOGIN_REDIRECT_URL)
-    
+
     if 'error' in request.GET:
-        messages.add_message(request, messages.ERROR, 
+        messages.add_message(request, messages.ERROR,
                              request.GET['error'])
         return HttpResponseRedirect(settings.LOGIN_REDIRECT_URL)
     elif 'code' in request.GET:
@@ -51,25 +51,17 @@ def login_handler(request, backend=None):
                                context_instance=RequestContext(request))
             response.status_code = 500
             return response
-        
+
         response = req.read()
         response_query_dict = json.loads(response)
         access_token = response_query_dict['access_token']
         expires_in = response_query_dict['expires_in']
 
-        #
-        #p #= #{#'#c#l#i#e#n#t#_#i#d#'#: #s#e#t#t#i#n#g#s#.#G#O#O#G#L#E#P#L#U#S#_#C#L#I#E#N#T#_#I#D#,
-                #'#c#l#i#e#n#t#_#s#e#c#r#e#t#'#: #s#e#t#t#i#n#g#s#.#G#O#O#G#L#E#P#L#U#S#_#C#L#I#E#N#T#_#S#E#C#R#E#T#,
-                #'#a#c#c#e#s#s#_#t#o#k#e#n#'#: #a#c#c#e#s#s#_#t#o#k#e#n#,
-                #'#s#c#o#p#e#'#: #'#h#t#t#p#s#:#/#/#w#w#w#.#g#o#o#g#l#e#a#p#i#s#.#c#o#m#/#a#u#t#h#/#u#s#e#r#i#n#f#o#.#e#m#a#i#l#' #}
-        #r#e#s #= #u#r#l#l#i#b#.#u#r#l#o#p#e#n#(#'#h#t#t#p#s#:#/#/#w#w#w#.#g#o#o#g#l#e#a#p#i#s#.#c#o#m#/#u#s#e#r#i#n#f#o#/#e#m#a#i#l#'#,
-                #u#r#l#l#i#b#.#u#r#l#e#n#c#o#d#e#(#p#a#r#a#m#s#)#)
-        #
-        
+
         profile = utils.api('people/me', {'access_token': access_token})
-        
+
         googleplus_user = _create_or_update_googleplus_user(request, profile, access_token, expires_in, backend)
-        
+
         user = authenticate(googleplus_user=googleplus_user)
         if user is not None:
             if user.is_active:
@@ -88,7 +80,7 @@ def login_handler(request, backend=None):
             'redirect_uri': REDIRECT_URI, \
             'scope': 'https://www.googleapis.com/auth/plus.me', \
             'response_type': 'code', \
-        }    
+        }
         return HttpResponseRedirect('https://accounts.google.com/o/oauth2/auth?' +
             urllib.urlencode(params)
         )
@@ -144,13 +136,8 @@ def _create_or_update_googleplus_user(request, profile, access_token, expires_in
         else:
             raise ValidationError('Something went wrong')
 
-        #u#s#e#r #= #U#s#e#r#.#o#b#j#e#c#t#s#.#c#r#e#a#t#e#( #\
-            #f#i#r#s#t#_#n#a#m#e#=#f#i#r#s#t#_#n#a#m#e#,
-            #l#a#s#t#_#n#a#m#e#=#l#a#s#t#_#n#a#m#e#,
-            #u#s#e#r#n#a#m#e#=#'#g#o#o#g#l#e#p#l#u#s#_#' #+ #p#r#o#f#i#l#e#[#'#i#d#'#]
-        #)
         user_is_created = True
-        
+
     if user_is_created:
         googleplus_user = profile_model()
         googleplus_user.googleplus_id = profile['id']
@@ -159,13 +146,13 @@ def _create_or_update_googleplus_user(request, profile, access_token, expires_in
         first_name, last_name = _get_first_and_last_name(profile['displayName'])
         googleplus_user.user.first_name = first_name
         googleplus_user.last_name = last_name
-        
+
     googleplus_user.googleplus_display_name = profile['displayName']
     googleplus_user.access_token = access_token
     googleplus_user.expiry_at = datetime.datetime.now() + \
-        datetime.timedelta(seconds=int(expires_in))    
+        datetime.timedelta(seconds=int(expires_in))
     googleplus_user.save()
-    
+
     return googleplus_user
 
 def _get_first_and_last_name(display_name):
